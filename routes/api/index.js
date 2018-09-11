57d87dcbabf4e9fb5b167daf3426784ce8d404d6
unknown <jkral1101@gmail.com>
2018-09-10 22:51:01 -0500

    build

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 
 
 router.get("/events:id", function(req, res) {
-  // apiKey: 'h3LjwpmzbpDcs6wx
+  // apiKey: 'h3LjwpmzbpDcs6wx.
   let searchTerm = req.params.id
   axios.get('http://api.eventful.com/json/events/search?app_key=h3LjwpmzbpDcs6wx&location=' + searchTerm + '&date=Future')
 
