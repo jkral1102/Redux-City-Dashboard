7423e1cc5ce010f5c212282c08cf5385c2cbba0f
unknown <jkral1101@gmail.com>
2018-09-10 22:50:08 -0500

    push build

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,17 +1,15 @@
 const router = require("express").Router();
 const noteRoutes = require("./notes");
-//import axios from 'axios'
 
 const axios = require('axios')
 
 
 router.get("/events:id", function(req, res) {
-  // fetch('http://api.eventful.com/json/events/search?app_key=' + apiKey + 'h3LjwpmzbpDcs6wx&location=' + searchTerm + '&date=Future',
+  // apiKey: 'h3LjwpmzbpDcs6wx
   let searchTerm = req.params.id
   axios.get('http://api.eventful.com/json/events/search?app_key=h3LjwpmzbpDcs6wx&location=' + searchTerm + '&date=Future')
 
   .then(response => res.json(response.data))
-  //.then(res => console.log(res))
 
   })
 
