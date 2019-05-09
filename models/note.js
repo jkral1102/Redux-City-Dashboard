bb85fdc3f2715a06cd2cd52df00fb871fa6c1fbd
unknown <jkral1101@gmail.com>
2019-05-08 22:48:32 -0500

    updated node modules

--- models/note.js
+++ models/note.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const noteSchema = new Schema({
-  //key: {type: Number },
-  //title: { type: String},
-  note: { type: String },
-  //date: { type: Date, default: Date.now },
-  // comments: [{
-  //   comment: { type: String }, 
-  //   time: {type: Date, default: Date.now }, 
-  //   username: {type: String}
-  // }],
+// const noteSchema = new Schema({
+//   //key: {type: Number },
+//   //title: { type: String},
+//   note: { type: String },
+//   //date: { type: Date, default: Date.now },
+//   // comments: [{
+//   //   comment: { type: String }, 
+//   //   time: {type: Date, default: Date.now }, 
+//   //   username: {type: String}
+//   // }],
 
-});
+// });
 
-const Note = mongoose.model("Note", noteSchema);
+// const Note = mongoose.model("Note", noteSchema);
 
-module.exports = Note;
+// module.exports = Note;
